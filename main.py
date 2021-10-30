0e43832ac1acc6e01ed1f46ded78b1e7db169d64
darcsoel <45120936+darcsoel@users.noreply.github.com>
2021-10-30 23:08:03 +0300

    Create pylint.yml

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import itertools
+from collections import Counter
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class BruteForceSolution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        if not s or not words:
+            return []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        words_combinations = itertools.permutations(words)
+        words_combinations = set(["".join(words) for words in words_combinations])
+        result = []
+
+        for word in words_combinations:
+            start = 0
+            while True:
+                try:
+                    start = s.index(word, start)
+                    result.append(start)
+                    start += 1
+                except ValueError:
+                    break
+
+        return result
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        result = []
+        origin, word_len, str_len = Counter(words), len(words[0]), len(s)
+        length = len(words)
+
+        for i in range(str_len - word_len * length + 1):
+            current = Counter(origin)
+            exists_in_counter = True
+
+            for j in range(i, i + word_len * length, word_len):
+                if not current[s[j:j + word_len]]:
+                    exists_in_counter = False
+                    break
+                else:
+                    current[s[j:j + word_len]] -= 1
+
+            if exists_in_counter:
+                result.append(i)
+
+        return result
--- tests.py
+++ tests.py
@@ -1,1 +1,54 @@
-import pytest
+# from main import BruteForceSolution as Solution
+from main import Solution
+
+
+def test_1():
+    result = Solution().findSubstring("barfoothefoobarman", ["foo", "bar"])
+    assert sorted(result) == [0, 9]
+
+
+def test_2():
+    result = Solution().findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "word"])
+    assert result == []
+
+
+def test_3():
+    result = Solution().findSubstring("barfoofoobarthefoobarman", ["bar", "foo", "the"])
+    assert sorted(result) == [6, 9, 12]
+
+
+def test_4():
+    result = Solution().findSubstring("foobarfoobar", ["foo", "bar"])
+    assert sorted(result) == [0, 3, 6]
+
+
+def test_5():
+    result = Solution().findSubstring("aaaaaaaaaaaaaa", ["aa", "aa"])
+    assert sorted(result) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_6():
+    result = Solution().findSubstring(
+        "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel",
+        [
+            "dhvf",
+            "sind",
+            "ffsl",
+            "yekr",
+            "zwzq",
+            "kpeo",
+            "cila",
+            "tfty",
+            "modg",
+            "ztjg",
+            "ybty",
+            "heqg",
+            "cpwo",
+            "gdcj",
+            "lnle",
+            "sefg",
+            "vimw",
+            "bxcb",
+        ],
+    )
+    assert sorted(result) == [935]
